559583bb2f3c3eacacea46317a888991ff75fae6
Markoyy <mcjusoy@gmail.com>
2024-10-04 10:24:16 -0700

    Initialize Flowbite

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,29 @@
+import flowbitePlugin from "flowbite/plugin";
+
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
-
+    content: ["./src/**/*.{html,js,svelte,ts}", "./node_modules/flowbite-svelte/**/*.{html,js,svelte,ts}"],
+    darkMode: "selector",
   theme: {
-    extend: {}
-  },
+        extend: {
+            colors: {
+                // flowbite-svelte
+                primary: {
+                    "50": "#FDF2F8",
+                    "100": "#FCE8F3",
+                    "200": "#FAD1E8",
+                    "300": "#F8B4D9",
+                    "400": "#F17EB8",
+                    "500": "#E74694",
+                    "600": "#D61F69",
+                    "700": "#BF125D",
+                    "800": "#99154B",
+                    "900": "#751A3D",
+                },
+            },
+        },
+    },
 
-  plugins: []
+    plugins: [flowbitePlugin],
 } as Config;
